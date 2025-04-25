d1de9cd6411ee8ee6faa4b2f2b5c54474bcbb795
Marijn Stollenga <m.stollenga@gmail.com>
2025-04-25 13:12:21 +0200

    merge

--- src/main.zig
+++ src/main.zig
@@ -17,11 +17,83 @@ pub fn tree_callback(root: [*c]const u8, entry: ?*const git.git_tree_entry, payl
     return 0;
 }
 
+var repo: *git.git_repository = undefined;
+const ally = std.heap.page_allocator;
+
+fn git_try(err_code: c_int) !void {
+    if (err_code < 0) {
+        return error.git_error;
+    }
+}
+
+pub fn get_dir(path: []const u8) !*git.git_tree {
+    const branch = "refs/heads/master";
+
+    var treeish: ?*git.git_object = null;
+    try git_try(git.git_revparse_single(&treeish, repo, branch));
+    defer git.git_object_free(treeish);
+
+    var commit: ?*git.git_commit = null;
+    try git_try(git.git_commit_lookup(&commit, repo, git.git_object_id(treeish)));
+
+    var tree: ?*git.git_tree = null;
+    try git_try(git.git_commit_tree(&tree, commit));
+
+    var current_tree = tree;
+    var it = std.mem.tokenize(u8, path, "/");
+
+    while (it.next()) |subpath| {
+        const subpath_z = try ally.dupeZ(u8, subpath);
+        defer ally.free(subpath_z);
+
+        const entry = git.git_tree_entry_byname(current_tree, subpath_z);
+        std.debug.print("found: {any}\n", .{entry});
+
+        const entry_type = git.git_tree_entry_type(entry);
+        if (entry_type != git.GIT_OBJ_TREE) {
+            return error.ExpectedTree;
+        }
+
+        const oid = git.git_tree_entry_id(entry);
+        try git_try(git.git_tree_lookup(&current_tree, repo, oid));
+    }
+
+    return current_tree.?;
+}
+
+pub fn init() !void {
+    _ = git.git_libgit2_init();
+
+    {
+        const path = ".";
+        var repo_tmp: ?*git.git_repository = null;
+        const err = git.git_repository_open(&repo_tmp, path);
+        if (err < 0) {
+            return error.Failed;
+        }
+        repo = repo_tmp.?;
+    }
+}
+
+pub fn deinit() void {
+    _ = git.git_libgit2_shutdown();
+
+    git.git_repository_free(repo);
+}
+
+pub fn list_git_dir(tree: *git.git_tree) void {
+    const N = git.git_tree_entrycount(tree);
+    for (0..N) |n| {
+        const entry = git.git_tree_entry_byindex(tree, n);
+        const name = git.git_tree_entry_name(entry);
+        std.log.info("entry: {s}", .{name});
+    }
+}
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    const ally = std.heap.page_allocator;
     var args = std.ArrayList([:0]u8).init(ally);
     {
         var iterator = try std.process.argsWithAllocator(ally);
@@ -30,95 +102,27 @@ pub fn main() !void {
             try args.append(try ally.dupeZ(u8, arg));
         }
     }
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
 
-    try stdout.print("Hello.\n", .{});
+    try init();
+    defer deinit();
 
-    _ = git.git_libgit2_init();
-    defer _ = git.git_libgit2_shutdown();
-
-    var repo: ?*git.git_repository = null;
-    var err = git.git_repository_open(&repo, ".");
-    if (err < 0) {
-        return error.Failed;
-    }
-    defer git.git_repository_free(repo);
-
-    var treeish: ?*git.git_object = null;
-    err = git.git_revparse_single(&treeish, repo, "refs/heads/master");
-    if (err < 0) {
-        return error.Failed;
-    }
-    defer git.git_object_free(treeish);
-
-    var commit: ?*git.git_commit = null;
-    err = git.git_commit_lookup(&commit, repo, git.git_object_id(treeish));
-    if (err < 0) {
-        return error.Failed;
-    }
-
-    var tree: ?*git.git_tree = null;
-    err = git.git_commit_tree(&tree, commit);
-    if (err < 0) {
-        return error.Failed;
-    }
-
-    {
-        const path = args.items[1];
-
-        var iterator = std.mem.tokenize(u8, path, "/");
-        while (iterator.next()) |name| {
-            if (std.mem.eql(u8, name, ".")) {
-                continue;
-            }
-            std.log.info("looking up: {s}", .{name});
-            const namez = try ally.dupeZ(u8, name);
-            const entry = git.git_tree_entry_byname(tree, namez);
-            if (entry == null)
-                return error.NotFound;
-
-            const entry_type = git.git_tree_entry_type(entry);
-
-            if (entry_type == git.GIT_OBJ_BLOB) {
-                return error.FoundBlob;
-            }
-
-            if (entry_type == git.GIT_OBJ_TREE) {
-                const oid = git.git_tree_entry_id(entry);
-                err = git.git_tree_lookup(&tree, repo, oid);
-                if (err < 0) {
-                    return error.Failed;
-                }
-            }
-        }
-        // std.debug.print("found: {any}\n", .{entry});
-
-        // std.debug.print("found: {s}\n", .{git.git_tree_entry_name(entry)});
-    }
-
+    const path = args.items[1];
+    const path_tree = try get_dir(path);
+    list_git_dir(path_tree);
     // _ = git.git_tree_walk(tree, git.GIT_TREEWALK_PRE, tree_callback, null);
 
-    const operations: fuse.fuse_operations = .{
-        .getattr = &getattr,
-        .readdir = &readdir,
-    };
+    // const operations: fuse.fuse_operations = .{
+    //     .getattr = &getattr,
+    //     .readdir = &readdir,
+    // };
 
-    // const argc: c_int = 0;
-    // const argv: [*c][*c]u8 = undefined;
-    var c_strings = try ally.alloc([*c]u8, args.items.len + 1);
-    for (0..args.items.len) |i| {
-        c_strings[i] = args.items[i];
-    }
-    c_strings[args.items.len] = null;
+    // var c_strings = try ally.alloc([*c]u8, args.items.len + 1);
+    // for (0..args.items.len) |i| {
+    //     c_strings[i] = args.items[i];
+    // }
+    // c_strings[args.items.len] = null;
 
-    _ = fuse.fuse_main_real(@intCast(args.items.len), @ptrCast(c_strings), &operations, @sizeOf(@TypeOf(operations)), null);
-
-    try bw.flush(); // don't forget to flush!
+    // _ = fuse.fuse_main_real(@intCast(args.items.len), @ptrCast(c_strings), &operations, @sizeOf(@TypeOf(operations)), null);
 }
 
 pub fn readdir(path: [*c]const u8, buf: ?*anyopaque, filler: fuse.fuse_fill_dir_t, offset: fuse.off_t, fi: ?*fuse.fuse_file_info, flags: fuse.fuse_readdir_flags) callconv(.C) c_int {
